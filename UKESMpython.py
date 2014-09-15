d9ea0fb0168523f01412992e399812a6ef6a07df
Lee <ledm@pml.ac.uk>
2014-09-15 11:38:13 +0100

    Got a functional analysis template up and running.

--- UKESMpython.py
+++ UKESMpython.py
@@ -1,5 +1,8 @@
 from sys import argv
-
+from string import join
+from os.path  import exists
+from os import mkdir, makedirs
+from glob import glob
 
 """	This is a catch all toolkit for the python methods and shorthands used in this code.
 """
@@ -7,9 +10,35 @@ from sys import argv
 
 
 
+
+
+def folder(name):
+	""" This snippet takes a string, makes the folder and the string.
+	    It also accepts lists of strings.
+	"""
+	if type(name) == type(['a','b','c']):
+		name=join(name,'/')
+	if name[-1] != '/':
+		name = name+'/'
+	if exists(name) is False:
+		makedirs(name)
+		print 'makedirs ', name
+	return name
+
+
 def getCommandJobIDandTime():
 	jobID = argv[1]	
 	timestamp = argv[2]
 	return jobID,timestamp
 	
+def getFileList(fin):
+	if type(fin)==type('abc') and fin.find('*')<0 and fin.find('?')<0: # fin is a string file:
+		return [fin,]
+	if type(fin)==type('abc') and (fin.find('*')>-1 or fin.find('?')>-1 or fin.find('[')>-1): # fin is a string file:
+		return glob(fin)
+	if type(fin) == type(['a','b','c',]): # fin is many files:
+		filesout = []
+		for f in fin:
+			filesout.extend(glob(f))
+		return filesout
 	
--- analysisTemplate.py
+++ analysisTemplate.py
@@ -1,26 +1,34 @@
 #!/usr/bin/ipython 
+#Standard Python modules:
 from netCDF4 import Dataset
-
+from sys import argv
+from matplotlib import pyplot
+#Specific local code:
 from pftnames import pftnames
-from UKESMpython import getCommandJobIDandTime
+from UKESMpython import folder,getFileList
 
 
 """	This is a template for performing an analsysis based on a single output file.
 """
 
 class analysistemplate:
-  def __init__(self,fileIn,)
-	getDateFromFile(filein)
-	getJobIDFromFile(filein)
+  def __init__(self,fileIn,):
+  	self.fileIn = fileIn
+	self.date	= 'date'	#getDateFromFile(filein)
+	self.jobID	= 'jobid'	#getJobIDFromFile(filein)
 
 	self.loadData()
 	self.makePlot()
 	
+	self.nc.close()
+	
   def loadData(self,):
   	self.nc = Dataset(self.fileIn,'r')
 
-
   def makePlot(self,):
+	
+	self.filename = folder('images/analysistemplate')+'_'.join(['analysistemplate',self.date,self.jobID])+'.png'
+	
   	fig = pyplot.figure()
   	ax = fig.add_subplot(111)
   	
@@ -28,10 +36,9 @@ class analysistemplate:
 
 	pyplot.xlabel('x axis label')
 	pyplot.ylabel('y axis label')
-	
 		
-  	print "analysistemplate:\tmakePlot\tSaving: " + self.filename
-	pyplot.savefig(filename,dpi=100,)# bbox_inches='tight')
+  	print "analysistemplate:\tINFO\tSaving: " + self.filename
+	pyplot.savefig(self.filename,dpi=100,)# bbox_inches='tight')
 	pyplot.close()	
 
 
@@ -41,29 +48,19 @@ class analysistemplate:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def main():
+	getFileList(argv[1:])
+	try:
+		filesIn = getFileList(argv[1:])
+		print "Using command line arguments:", filesIn	
+		
+	except:
+		print "analysistemplate:\tERROR:\tNo file specified"
+		return
+		
+	
+	for fn in filesIn:
+		a = analysistemplate(fn)
 
 
 
@@ -73,4 +70,4 @@ class analysistemplate:
 	
 if __name__=="__main__":
 	main() 
-	print 'The end.'
+	#print 'The end.'
