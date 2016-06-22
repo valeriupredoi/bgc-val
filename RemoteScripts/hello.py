8fe2e5f2bcd3a3d61131ca6969020867b81be1dc
Lee de Mora <ledm@pml.ac.uk>
2016-06-22 17:06:54 +0100

    created new remotes scripts to run u-ad980 on jasmin from remotes.

---
+++ RemoteScripts/hello.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+from socket import gethostname
+print "Hello world from ",gethostname()
---
+++ RemoteScripts/runWholePackageOnSci1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ssh jasmin-sci1 'python /home/users/ldemora/testcron/hello.py'
+ssh jasmin-sci1 -X 'ipython /home/users/ldemora/workspace/ukesm-validation/theWholePackage.py u-ad980'
