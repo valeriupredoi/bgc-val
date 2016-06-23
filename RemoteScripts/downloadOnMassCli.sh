1800e207561c91c3b02b1ef76a4335189a8485f9
Lee <ledm@pml.ac.uk>
2016-06-23 14:12:23 +0100

    added the script that pmlcron calls, and made debug changes to download

--- RemoteScripts/downloadOnMassCli.sh
+++ RemoteScripts/downloadOnMassCli.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-ssh mass-cli1 'python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py'
+echo "uname -a"
+uname -a 
+echo "USER: " $USER
+#eval "$(ssh-agent)"
 
-ssh mass-cli1 '/home/users/ldemora/workspace/ukesm-validation/downloadFromMass.py u-ad980'
+echo 'SSH_AUTH_SOCK:' $SSH_AUTH_SOCK  
+echo 'SSH_CLIENT:' $SSH_CLIENT 
+echo 'SSH_CONNECTION:' $SSH_CONNECTION 
+echo 'SSH_TTY:'  $SSH_TTY        
+ 
+python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py
+
+
+#printf "\n\nssh -A -X mass-cli1 'python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py'\n"
+#ssh -X -A mass-cli1 'python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py'
+
+ssh -X -A mass-cli1 'python /home/users/ldemora/workspace/ukesm-validation/downloadFromMass.py u-ad980'
+
 
---
+++ RemoteScripts/pmlcron_hello.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source /users/modellers/ledm/.bashrc
+. ~/.keychain/$HOSTNAME-sh
+
+export XAUTHORITY=/users/modellers/ledm/.Xauthority
+export DISPLAY=':0'
+#eval `ssh-agent -s` 
+ssh -A -X ldemora@jasmin-login1.ceda.ac.uk '/home/users/ldemora/workspace/ukesm-validation/RemoteScripts/helloOnMass.sh' 
+#kill $SSH_AGENT_PID
+
