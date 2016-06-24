0d9349a21447efb0836a59cbbf5a999a99871764
Lee <ledm@pml.ac.uk>
2016-06-24 15:37:35 +0100

    de ug 1446-1448.

--- RemoteScripts/runReportOnSci1.sh
+++ RemoteScripts/runReportOnSci1.sh
@@ -13,6 +13,6 @@ python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py
 
 ssh -X -A jasmin-sci2 'cd /home/users/ldemora/workspace/ukesm-validation; ipython /home/users/ldemora/workspace/ukesm-validation/theWholePackage.py u-ad980 ReportOnly'
 
-rsync -avP /home/users/ldemora/workspace/ukesm-validation/report-u-ad980.tar.gz ledm@pmpc1446.npm.ac.uk:~/ImagesFromJasmin/.
+rsync -avP /home/users/ldemora/workspace/ukesm-validation/report-u-ad980.tar.gz ledm@pmpc1448.npm.ac.uk:~/ImagesFromJasmin/.
 
 echo "The end of runReportOnSci1.sh"
