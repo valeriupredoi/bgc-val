2c5ebe73b4efe0b17d194def76dc9ef5c82d8975
Lee de Mora <ledm@pml.ac.uk>
2016-07-11 16:49:35 +0100

    added command line work to take jobID

--- RemoteScripts/helloOnMass.sh
+++ RemoteScripts/helloOnMass.sh
@@ -12,7 +12,13 @@ echo 'SSH_TTY:'  $SSH_TTY
 #rintf "\n\nssh-add -l\n"
 #sh-add -l
 
-python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py
+#####
+# parsing job id
+jobid=${1:-u-ad980}
+echo jobid=$jobid
+export jobid=$jobid
+
+python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py $jobid
 
 #printf "\n\nssh -v -X -i /home/users/ldemora/.ssh/id_rsa ldemora@mass-cli1 'python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py'\n"
 #ssh -v -X -A -i /home/users/ldemora/.ssh/id_rsa ldemora@mass-cli1 'source /home/users/ldemora/.bashrc; python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py'
@@ -21,9 +27,9 @@ python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py
 #ssh -v -X -A ldemora@mass-cli1 'python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py'
 
 printf "\n\nssh -A -X mass-cli1 'python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py'\n"
-ssh -X -A mass-cli1 'python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py'
+ssh -X -A mass-cli1 "python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py $jobid"
 
-ssh -X -A jasmin-sci1 'python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py'
+ssh -X -A jasmin-sci1 "python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py $jobid"
 
 
 
