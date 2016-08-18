160fadd18e5af68ed20f1b08a87f2710b7ce5f59
Lee <ledm@pml.ac.uk>
2016-08-18 14:56:35 +0100

    added organge spelling

--- analysis_compare.py
+++ analysis_compare.py
@@ -53,7 +53,7 @@ def timeseries_compare():
 	
 	#jobs = ['u-af123', 'u-af139','u-af420','u-af421', ]
 	jobs = ['u-af725', 'u-af728','u-af420','u-af421', 'u-af586','u-af588',]	
-	colours = {'u-af725':'red', 'u-af728':'organge','u-af420':'greenyellow','u-af421':'purple', 'u-af586':'green','u-af588':'blue',}		
+	colours = {'u-af725':'red', 'u-af728':'orange','u-af420':'greenyellow','u-af421':'purple', 'u-af586':'green','u-af588':'blue',}		
 	annual = True
 	
 	analysisKeys = []
