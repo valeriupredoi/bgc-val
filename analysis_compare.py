1f0c7d87bb650d74fc7bbbfd95db5683c9eeb759
Lee <ledm@pml.ac.uk>
2016-08-30 10:23:43 +0100

    added latest jobIDs and colour scheme.

--- analysis_compare.py
+++ analysis_compare.py
@@ -52,8 +52,9 @@ def timeseries_compare():
 	
 	
 	#jobs = ['u-af123', 'u-af139','u-af420','u-af421', ]
-	jobs = ['u-af725', 'u-af728','u-af420','u-af421', 'u-af586','u-af730','u-ae748']	
-	colours = {'u-af725':'red', 'u-af728':'orange','u-af420':'blue','u-af421':'purple', 'u-af586':'green','u-af730':'pink','u-ae748':'black'}
+
+	colours = {'u-af930':'red', 'u-af921':'orange','u-af748':'darkblue','u-af915':'blue','u-af914':'purple', }#'u-af586':'green','u-af730':'pink','u-ae748':'black'}
+	jobs = sorted(colours.keys())#['u-af725', 'u-af728','u-af420','u-af421', 'u-af586','u-af730','u-ae748']	
 	annual = True
 	strictFileCheck = False
 
