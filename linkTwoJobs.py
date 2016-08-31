829021388c5a608cc7d031f687887a79b3706034
Lee de Mora <ledm@pml.ac.uk>
2016-08-31 11:15:25 +0100

    local changes on jasmin sent to pml.

--- linkTwoJobs.py
+++ linkTwoJobs.py
@@ -70,9 +70,9 @@ def linkTwoJobs(jobID1,jobID2):
 				print netcdfFold1
 				return
 			if not os.path.exists(netcdfFold2):
-				print "This folder doesn't exist. Do you have the right jobID 2?", jobID2
-                                print netcdfFold2
-				return						
+				print "This folder doesn't exist yet."
+                                print "making", folder(netcdfFold2)
+				#return						
 			shelvefold1 = "/group_workspaces/jasmin2/ukesm/BGC_data/"+getuser()+"/shelves/*/"+jobID1
 			shelvefold2 = "/group_workspaces/jasmin2/ukesm/BGC_data/"+getuser()+"/shelves/*/"+jobID2
 		
