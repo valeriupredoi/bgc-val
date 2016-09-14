77219d902a37d1962bd04bc88ba5d4084fac0197
Lee <ledm@pml.ac.uk>
2016-09-14 14:35:58 +0100

    added new analysis files for Level 0.

---
+++ Paths/paths_jasmin.py
@@ -0,0 +1,72 @@
+#
+# Copyright 2015, Plymouth Marine Laboratory
+#
+# This file is part of the bgc-val library.
+#
+# bgc-val is free software: you can redistribute it and/or modify it
+# under the terms of the Revised Berkeley Software Distribution (BSD) 3-clause license. 
+
+# bgc-val is distributed in the hope that it will be useful, but
+# without any warranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the revised BSD license for more details.
+# You should have received a copy of the revised BSD license along with bgc-val.
+# If not, see <http://opensource.org/licenses/BSD-3-Clause>.
+#
+# Address:
+# Plymouth Marine Laboratory
+# Prospect Place, The Hoe
+# Plymouth, PL1 3DH, UK
+#
+# Email:
+# ledm@pml.ac.uk
+
+
+from socket import gethostname
+import UKESMpython as ukp
+from getpass import getuser
+
+#####
+# JASMIN
+machinelocation = ''
+
+#####
+# JASMIN		
+if gethostname().find('ceda.ac.uk')>-1:
+	print "analysis-timeseries.py:\tBeing run at CEDA on ",gethostname()
+	machinelocation = 'JASMIN'
+			
+	#####
+	# Post processed Data location
+	shelvedir 	= ukp.folder("/group_workspaces/jasmin2/ukesm/BGC_data/"+getuser()+"/shelves/")
+		
+	#####
+	# Location of model files.
+	esmvalFolder 	= "/group_workspaces/jasmin2/ukesm/BGC_data/"
+	ModelFolder_pref	= ukp.folder(esmvalFolder)
+
+	#####
+	# eORCA1 grid		
+	orcaGridfn 	= '/group_workspaces/jasmin/esmeval/example_data/bgc/mesh_mask_eORCA1_wrk.nc'
+	
+	#####
+	# Location of data files.
+	ObsFolder 	= "/group_workspaces/jasmin/esmeval/example_data/bgc/"
+	WOAFolder_annual= ObsFolder+"WOA/annual/"
+	WOAFolder 	= ObsFolder+"WOA/"
+	MAREDATFolder 	= ObsFolder+"/MAREDAT/MAREDAT/"
+	GEOTRACESFolder = ObsFolder+"/GEOTRACES/GEOTRACES_PostProccessed/"
+	TakahashiFolder = ObsFolder+"/Takahashi2009_pCO2/"
+	MLDFolder	= ObsFolder+"/IFREMER-MLD/"
+	iMarNetFolder	= ObsFolder+"/LestersReportData/"
+	GlodapDir	= ObsFolder+"/GLODAP/"
+	GLODAPv2Dir	= ObsFolder+"/GLODAPv2/GLODAPv2_Mapped_Climatologies/"
+	OSUDir		= ObsFolder+"OSU/"
+	CCIDir		= ObsFolder+"CCI/"
+	icFold		= ObsFolder+"/InitialConditions/"
+
+print "There may be problems with shelvedir (missing timeseries/jobID at the end) and WOAFolder, which is now WOAFolder and WOAFolder_annual"
+		
+		
+		
+		
+		
