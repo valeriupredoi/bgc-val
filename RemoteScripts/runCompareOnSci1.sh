89bfae6ff8c2b1dabc730184f6a6bc699329ac9c
Lee de Mora <ledm@pml.ac.uk>
2017-02-02 10:49:56 +0000

    fixed problem so that these can run from cronjob.

--- RemoteScripts/runCompareOnSci1.sh
+++ RemoteScripts/runCompareOnSci1.sh
@@ -11,9 +11,11 @@ echo 'SSH_TTY:'  $SSH_TTY
 
 #####
 # parsing job id
-#jobid=${1:-u-ad980}
-#echo jobid=$jobid
-#export jobid=$jobid
+jobid=${1:-u-ad980}
+echo jobid=$jobid
+export jobid=$jobid
+
+python /home/users/ldemora/workspace/ukesm-validation/RemoteScripts/hello.py $jobid
 
 ssh -X -A jasmin-sci1 "cd /home/users/ldemora/workspace/ukesm-validation; ipython /home/users/ldemora/workspace/ukesm-validation/analysis_compare.py; rsync -a /home/users/ldemora/workspace/ukesm-validation/images/TimeseriesCompare/* /group_workspaces/jasmin/esmeval/public/Intercomparison/."
 echo "The end of runCompareOnSci1.sh on login lander"
--- analysis_compare.py
+++ analysis_compare.py
@@ -30,6 +30,10 @@
 
 """
 
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+
 #####	
 # Load Standard Python modules:
 from sys import argv,exit
