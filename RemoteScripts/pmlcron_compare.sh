a0c9c3b67ab2503e0c2456e4fe4c22b7e4ef1526
Lee de Mora <ledm@pml.ac.uk>
2018-05-23 09:26:46 +0100

    removed undeeded jobID

--- RemoteScripts/pmlcron_compare.sh
+++ RemoteScripts/pmlcron_compare.sh
@@ -8,11 +8,11 @@
 
 #####
 # parsing job id
-jobid=${1:-compare}
-echo jobid=$jobid
-export jobid=$jobid
+#jobid=${1:-compare}
+#echo jobid=$jobid
+#export jobid=$jobid
 
 #ssh -A -X ldemora@jasmin-login1.ceda.ac.uk "/home/users/ldemora/workspace/ukesm-validation/RemoteScripts/helloOnMass.sh $jobid"
 
-ssh -A -X ldemora@jasmin-login1.ceda.ac.uk "/home/users/ldemora/workspace/ukesm-validation/RemoteScripts/runCompareOnSci1.sh $jobid;"
+ssh -A -X ldemora@jasmin-login1.ceda.ac.uk "/home/users/ldemora/workspace/ukesm-validation/RemoteScripts/runCompareOnSci1.sh"
 
