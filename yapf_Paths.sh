c6c4e6a12f22784a653b39007ee4b40ca1054d3f
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-04-20 14:37:39 +0100

    cleaning up

--- yapf_Paths.sh
+++
@@ -1,4 +0,0 @@
-yapf -i Paths/paths_jasmin.py
-yapf -i Paths/paths_monsoon.py
-yapf -i Paths/paths_pml.py
-yapf -i Paths/paths_template.py
--- yapf_timeseries.sh
+++
@@ -1,7 +0,0 @@
-yapf -i timeseries/analysis_level0.py
-yapf -i timeseries/extentMaps.py
-yapf -i timeseries/extentProfiles.py
-yapf -i timeseries/profileAnalysis.py
-yapf -i timeseries/timeseriesAnalysis.py
-yapf -i timeseries/timeseriesPlots.py
-yapf -i timeseries/timeseriesTools.py
