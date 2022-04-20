b14d0b6a5a8d72e442813bd9e03d5ff7e9d52efb
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-04-20 12:57:59 +0100

    added yapf sheller

---
+++ yapf_netcdf_manipulation.sh
@@ -0,0 +1,7 @@
+yapf -i netcdf_manipulation/mergeNC.py
+yapf -i netcdf_manipulation/alwaysInclude.py
+yapf -i netcdf_manipulation/changeNC.py
+yapf -i netcdf_manipulation/convertToOneDNC.py
+yapf -i netcdf_manipulation/depthManipNC.py
+yapf -i netcdf_manipulation/depthProfileNC.py
+yapf -i netcdf_manipulation/pruneNC.py
