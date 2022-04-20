382d811a365f1faebbfa793a142b4408578a74aa
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-04-20 12:44:01 +0100

    temp scripts that yapf a lot of crap

---
+++ yapf_timeseries.sh
@@ -0,0 +1,7 @@
+yapf -i timeseries/analysis_level0.py
+yapf -i timeseries/extentMaps.py
+yapf -i timeseries/extentProfiles.py
+yapf -i timeseries/profileAnalysis.py
+yapf -i timeseries/timeseriesAnalysis.py
+yapf -i timeseries/timeseriesPlots.py
+yapf -i timeseries/timeseriesTools.py
